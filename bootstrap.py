0611897830b6bebfc5b1fce39be6c48e435732a0
NerdyBuzz <mohamedbashar98@gmail.com>
2015-10-17 14:59:20 -0400

    fix

--- bootstrap.py
+++ bootstrap.py
@@ -1,6 +1,6 @@
 import sys
 import os
-sys.path.append(os.path.abspath("HyphanBot/"))
+sys.path.append(os.path.abspath("HyphanBot/HyBotLauncher.py"))
 from HyBotLauncher import *
 
 launchBot(False, "HyphanBot/")
