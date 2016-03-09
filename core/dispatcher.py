597139f348b13d150675d608a0ea62459b7067ce
Valentijn <valentijn@openmailbox.org>
2016-03-09 23:20:31 +0100

    Report errors while loading mods without crashing.

--- core/dispatcher.py
+++ core/dispatcher.py
@@ -17,28 +17,32 @@ https://www.gnu.org/licenses/agpl-3.0.html>.
 
 from constants import HYPHAN_DIR
 from modloader import *
-
 import logging
-
-global logger
-logger = logging.getLogger(__name__)
+import traceback
 
 def loadModules(api, updater):
-        # Get, load, and dispatch all mods found in the modules folder
-        for i in getMods(logger):
-                # Check if mod is enabled in config. If so, call its dispatch(). It's enabled by default.
-                modenabled = bool(api.config.config.getboolean(i['name'], "enabled", fallback="true"))
-                if modenabled:
-                        logger.info("Mod %s has been loaded." % i['name'])
-                        # This does some weird shinanagins.
-                        mod = loadMod(i) # Load the mod
-                        if "dispatch" in dir(mod): # Check if the dispatch function exists in the mod
-                                modapi = api.Mod(api, mod.__name__)
-                                os.chdir(i['location'])
-                                mod.dispatch(modapi, updater)
-                                os.chdir(HYPHAN_DIR+"/core")
-                        else:
-                                logger.warn("Cannot dispatch mod '%s': dispatch() is missing." % mod.__name__)
+    logger = logging.getLogger(__name__)
+    # Get, load, and dispatch all mods found in the modules folder
+    for i in getMods(logger):
+        # Check if mod is enabled in config. If so, call its dispatch().
+        # It's enabled by default.
+        modenabled = bool(api.config.config.getboolean(i['name'], "enabled", fallback="true"))
+        if modenabled:
+            # This does some weird shenanigans.
+            try:
+                mod = loadMod(i) # Load the mod
+                logger.info("Mod %s has been loaded." % i['name'])
+            except:
+                logger.error("Cannot load mod %s." % i['name'])
+                print(traceback.format_exc())
 
-                else:
-                        logger.warn("Mod %s is disabled." % i['name'])
+            if "dispatch" in dir(mod): # Check if the dispatch function exists in the mod
+                modapi = api.Mod(api, mod.__name__)
+                os.chdir(i['location'])
+                mod.dispatch(modapi, updater)
+                os.chdir(HYPHAN_DIR+"/core")
+            else:
+                logger.warn("Cannot dispatch mod '%s': dispatch() is missing." % mod.__name__)
+
+        else:
+            logger.warn("Mod %s is disabled." % i['name'])
